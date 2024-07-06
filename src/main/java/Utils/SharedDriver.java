df01b3b879306d5df46684364dabd0555be368c4
Oleksandr Oleksiienko <alexseenko00@gmail.com>
2024-07-06 13:35:54 +0300

    first commit

---
+++ src/main/java/Utils/SharedDriver.java
@@ -0,0 +1,62 @@
+package Utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SharedDriver {
+
+    private static WebDriver webDriver;
+
+    public enum Broswer {
+        CHROME,
+        FIREFOX,
+        IE
+    }
+
+
+    protected static WebDriver getWebDriver(Broswer broswer) {
+
+        switch (broswer){
+
+            case CHROME:
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--headless");
+
+
+                WebDriverManager.chromedriver().setup();
+                webDriver = new ChromeDriver(options);
+                break;
+
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                webDriver = new FirefoxDriver();
+                break;
+
+            case IE:
+                WebDriverManager.iedriver().setup();
+                webDriver = new InternetExplorerDriver();
+                break;
+        }
+
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        return webDriver;
+    }
+
+    protected static void closeDriver(){
+        if(webDriver!=null) {
+            webDriver.close();
+        }
+    }
+
+
+}
